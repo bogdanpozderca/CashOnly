132dc87ee16cb0ba54e9e4345aeb8ece353847fc
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-08-15 14:20:04 -0700

    menu bar slides down instead of from the side

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,6 @@ if (currentUser) {
 } 
 
 $( document ).ready(function() {
-    $('#simple-menu').sidr();
 
 	var refreshIntervalID = setInterval(updateUserLocation, 10000);
 
@@ -92,6 +91,35 @@ $( document ).ready(function() {
         $('#logged-in').fadeOut('slow'); $('#home').fadeIn('slow');
         
     });
+
+    $(function() {
+        var Accordion = function(el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
+
+            // Variables privadas
+            var links = this.el.find('.link');
+            // Evento
+            links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+        }
+
+        Accordion.prototype.dropdown = function(e) {
+            var $el = e.data.el;
+                $this = $(this),
+                $next = $this.next();
+
+            $next.slideToggle();
+            $this.parent().toggleClass('open');
+
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+            };
+        }   
+
+        var accordion = new Accordion($('#accordion'), false);
+    });    
+
+
 });
 
 
